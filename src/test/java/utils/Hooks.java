c5eaa6b83c0849b60413235ed24caf478683478c
anastasiia0706 <anastasiia.r0706@gmail.com>
2025-07-01 11:26:20 +0200

    updated ChromeOptions options

--- src/test/java/utils/Hooks.java
+++ src/test/java/utils/Hooks.java
@@ -24,17 +24,17 @@ public class Hooks {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
+        options.addArguments("--headless=new");
         options.addArguments("--no-sandbox"); // Prevent Linux sandbox errors
         options.addArguments("--disable-dev-shm-usage"); // Avoid shared memory issues
         options.addArguments("--disable-gpu"); // Optional but safe
         options.addArguments("--remote-allow-origins=*"); // For Chrome 111+
 
         // Unique temporary user data dir
-        Path tempProfileDir = Files.createTempDirectory("chrome-profile");
-        options.addArguments("--user-data-dir=" + tempProfileDir.toAbsolutePath());
+        Path tempProfile = Files.createTempDirectory("chrome-profile");
+        options.addArguments("--user-data-dir=" + tempProfile.toFile().getAbsolutePath());
 
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com");
         loginPage = new LoginPage(driver);
